33fe00bb6db67600c274f245cf10826eb0eab230
Ryan Luk <ryanluk4@gmail.com>
2020-02-07 15:00:16 -0800

    new temp/humid/gas/pressure and uv sensors

--- README.md
+++ README.md
@@ -2,13 +2,11 @@
 
 This is Arduino code for an Ethanol Sensor being designed by Peter Weiss and team.
 
-As of (2/3/2020) the code displays a timestamp and relative humidity from a sensor via analog input.
+As of (2/3/2020) the code displays a timestamp and relative humidity from a sensor (via analog input) on an LCD.
 
-Contributors are:
-Sharukh "Sharky" Lal
-Ryan Luk
-James Zhou
+Team members include:
+Sharukh "Sharky" Lal, Ryan Luk, James Zhou
 
-# Objective: 
+# Objective
 
 Design an ethanol sensor product that can be used in wineries to regulate ethanol concentrations.
--- sensor.ino
+++ sensor.ino
@@ -1,30 +1,28 @@
+// Temperature, humidity, pressure, gas sensor
 #include <Wire.h>
-#include <SparkFun_HIH4030.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BME680.h"
+
+#define BME_SCK 13 //21
+#define BME_MISO 12 //not using
+#define BME_MOSI 11 //20
+#define BME_CS 10 //not using
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME680 bme; // I2C
+//Adafruit_BME680 bme(BME_CS); // hardware SPI
+//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
+
 
 // Date and time functions using a PCF8523 RTC connected via I2C and Wire lib
 #include "RTClib.h"
-
 RTC_PCF8523 rtc;
-
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
+// LCD
 #include <LiquidCrystal.h>
-
-// Example Variables
-int tempAddress = 0x48;             // I2C: TMP102 Slave Address with ADD0 Grounded
-float temp = 0;                     // Temperature Value either Static or Sensor Reading
-
-// Are You Using a Temperature Sensor? 1 = YES / 0 = NO
-int tempSensor = 0;
-
-// Analog IO Pin Connected to OUT
-#define HIH4030_OUT A0
-
-// Supply Voltage - Typically 5 V
-#define HIH4030_SUPPLY 5
-
-// Library Variables
-HIH4030 sensorSpecs(HIH4030_OUT, HIH4030_SUPPLY);
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -33,13 +31,12 @@ void setup() {
   while (!Serial) {
     delay(1);  // for Leonardo/Micro/Zero
   }
-
+ 
   Serial.begin(57600);
   if (! rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
-
   if (! rtc.initialized()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
@@ -49,13 +46,23 @@ void setup() {
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
 
+  if (!bme.begin()) {
+    Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
+
   //initializes 20 columns and 4 rows
   lcd.begin(20, 4);
 
   //sets the cursor to first column and first row
   lcd.setCursor(0, 0);
-
-  Wire.begin();
 
   //print
   //lcd.print("Eth:");
@@ -71,30 +78,30 @@ void setup() {
 
 }
 
-/* Serial Output of Temperature C, Sensor Voltage V, SensorRH %, and TrueRH % */
-void printData(HIH4030 sensor, float temperature){
-  
-  Serial.print("Temperature = ");
-  Serial.print(temperature);
-  Serial.print(" C ("); Serial.print((temperature * 9/5) + 32); Serial.println(" F)");
-  Serial.print("Sensor Voltage = ");
-  Serial.print(sensor.vout());
-  Serial.println(" V");
-  Serial.print("Relative Humidity = ");
-  Serial.print(sensor.getSensorRH());
-  Serial.println(" %");
-  Serial.print("True Relative Humidity = ");
-  Serial.print(sensor.getTrueRH(temperature));
-  Serial.println(" %");
-  
-}
-
 void loop() {
+
+  float sensorVoltage;
+  float sensorValue;
+  
+  sensorValue = analogRead(A0);
+  sensorVoltage = sensorValue / 1023 * 3.3;
 
   //print
   lcd.setCursor(0, 0);
 
   DateTime now = (rtc.now() - TimeSpan(0,1,26,0));
+
+  // Tell BME680 to begin measurement.
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading :("));
+    return;
+  }
+
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading :("));
+    return;
+  }
 
   if(now.second() == 0){
     
@@ -104,11 +111,6 @@ void loop() {
     lcd.print(now.minute(), DEC);
     lcd.print(':');
     lcd.print(now.second(), DEC);
-
-    lcd.setCursor(0,1);
-    lcd.print(sensorSpecs.getTrueRH(25));
-    lcd.setCursor(5,1);
-    lcd.print("%");
     
   }
 
@@ -120,52 +122,33 @@ void loop() {
     lcd.print(':');
     lcd.print(now.second(), DEC);
 
-    lcd.setCursor(0,1);
-    lcd.print(sensorSpecs.getTrueRH(25));
-    lcd.setCursor(5,1);
-    lcd.print("%");
-
   }
 
-  /*  IF tempSensor = 1 Utilizing a Temperature Sensor             */
-  /*  IF tempSensor = 0 Utilizing a Static Value for Temperature   */
-  if (tempSensor == 1) {
-    temp = getTemperature();        // Get Temperature Sensor Reading
-  } else if (tempSensor == 0) {
-    temp = 25;                      // Static Temperature Value in C
-                                    // Set by User to Desired Temperature                            
-  } else {
-    while (tempSensor != 1 || tempSensor != 0){
-      Serial.println("ERROR: tempSensor Value Out of Range");
-    }
-  }
+  lcd.setCursor(0,1);
+  lcd.print(bme.temperature);
+  lcd.setCursor(6,1);
+  lcd.print("*C");
 
-  printData(sensorSpecs, temp);     // Print Sensor Readings
-  Serial.println("");               // Return Space Between Readings
+  lcd.setCursor(9,1);
+  lcd.print(bme.humidity);
+  lcd.setCursor(15,1);
+  lcd.print("%");
+
+  lcd.setCursor(0,2);
+  lcd.print(bme.pressure / 100.0);
+  lcd.setCursor(7,2);
+  lcd.print("hPa");
+
+  lcd.setCursor(0,3);
+  lcd.print(bme.gas_resistance / 1000.0);
+  lcd.setCursor(7,3);
+  lcd.print("KOhms");
+
+  lcd.setCursor(9,0);
+  lcd.print(sensorVoltage);
+  lcd.setCursor(14,0);
+  lcd.print("V");
 
   delay(1000);
-  
-}
-
-/* Only Called IF Temperature Sensor is Being Used */
-float getTemperature(){
-
-  /*  IF Temperature Sensor is different from the TMP102 and not   */      
-  /*    utilizing I2C via the Wire.h library, replace the code     */ 
-  /*    within the getTemperature() function.                      */ 
-  
-  Wire.requestFrom(tempAddress,2);  // Wire.requestFrom(address, # of bytes to request)
-
-  byte MSB = Wire.read();           // Receive byte as MSB or left-most bit
-  byte LSB = Wire.read();           // Receive byte as LSB or right-most bit
-
-  /*  TMP102 Temperature Register 12-bit, read-only, two's compliment for negative */
-  /*  << is bit shift left ; >> is bit shift right ; | is bitwise OR               */
-  /*  Syntax:   variable << number_of_bits                                         */
-  /*            variable >> number_of_bits                                         */
-  int TemperatureSum = ((MSB << 8) | LSB) >> 4; 
-
-  float celsius = TemperatureSum*0.0625;  //One LSB equals 0.0625 C
-  return celsius;
   
 }
