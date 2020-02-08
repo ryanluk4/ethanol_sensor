f55ad40b26bed62d2878f7106c5919abed5dc08e
Ryan Luk <ryanluk4@gmail.com>
2020-02-07 18:30:25 -0800

    updated readme and cleaned up code

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Ethanol Sensor
 
-This is Arduino code for an Ethanol Sensor being designed by Peter Weiss and team.
+This is Arduino code for an ethanol sensor being designed by Peter Weiss and team.
 
-As of (2/3/2020) the code displays a timestamp and relative humidity from a sensor (via analog input) on an LCD.
-
-Team members include:
-Sharukh "Sharky" Lal, Ryan Luk, James Zhou
+Team members include: James Zhou, Ryan Luk, Sharukh "Sharky" Lal
 
 # Objective
 
 Design an ethanol sensor product that can be used in wineries to regulate ethanol concentrations.
+
+# Components
+
+[Adafruit BME680](https://www.adafruit.com/product/3660) - Temperature, Humidity, Pressure, and Gas Sensor
+> Measures temperature, relative humidity, atmospheric pressure, and gas (VOC) resistance.
+
+[Adafruit GUVA-S12SD](https://www.adafruit.com/product/1918) - UV Sensor
+> Measures UV light (240-370nm) via analog input. Output is a voltage, which can be converted to UV Index by dividing by 0.1V.
+
+[Adafruit PCF8523](https://www.adafruit.com/product/3295) - Real time clock (RTC)
+> Real time clock for timing and date purposes.
--- sensor.ino
+++ sensor.ino
@@ -1,39 +1,36 @@
-// Temperature, humidity, pressure, gas sensor
+// BME
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
-
 #define BME_SCK 13 //21
 #define BME_MISO 12 //not using
 #define BME_MOSI 11 //20
 #define BME_CS 10 //not using
-
 #define SEALEVELPRESSURE_HPA (1013.25)
-
 Adafruit_BME680 bme; // I2C
 //Adafruit_BME680 bme(BME_CS); // hardware SPI
 //Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
 
-
-// Date and time functions using a PCF8523 RTC connected via I2C and Wire lib
+// RTC
 #include "RTClib.h"
 RTC_PCF8523 rtc;
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 // LCD
 #include <LiquidCrystal.h>
-
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
 
   while (!Serial) {
-    delay(1);  // for Leonardo/Micro/Zero
+    delay(1);
   }
  
   Serial.begin(57600);
-  if (! rtc.begin()) {
+
+  // RTC
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     while (1);
   }
@@ -46,84 +43,63 @@ void setup() {
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
 
+  // BME
   if (!bme.begin()) {
     Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
     while (1);
   }
-
-  // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
 
-  //initializes 20 columns and 4 rows
+  // LCD
   lcd.begin(20, 4);
-
-  //sets the cursor to first column and first row
   lcd.setCursor(0, 0);
-
-  //print
-  //lcd.print("Eth:");
-
-  //sets the cursor to first column and third row
-  //lcd.setCursor(0, 2);
-
-  //print
-  //lcd.print("Air:");
-
-  //begin transmitting analog voltage data
-  //Serial.begin(9600);
 
 }
 
 void loop() {
 
+  // UV
   float sensorVoltage;
   float sensorValue;
-  
   sensorValue = analogRead(A0);
   sensorVoltage = sensorValue / 1023 * 3.3;
 
-  //print
-  lcd.setCursor(0, 0);
-
-  DateTime now = (rtc.now() - TimeSpan(0,1,26,0));
-
-  // Tell BME680 to begin measurement.
+  // BME
   unsigned long endTime = bme.beginReading();
   if (endTime == 0) {
     Serial.println(F("Failed to begin reading :("));
     return;
   }
-
   if (!bme.endReading()) {
     Serial.println(F("Failed to complete reading :("));
     return;
   }
 
+  lcd.setCursor(0, 0);
+
+  // RTC
+  DateTime now = (rtc.now() - TimeSpan(0,1,26,0));
   if(now.second() == 0){
-    
     lcd.clear();
     lcd.print(now.hour(), DEC);
     lcd.print(':');
     lcd.print(now.minute(), DEC);
     lcd.print(':');
-    lcd.print(now.second(), DEC);
-    
+    lcd.print(now.second(), DEC); 
   }
-
   else{
-
     lcd.print(now.hour(), DEC);
     lcd.print(':');
     lcd.print(now.minute(), DEC);
     lcd.print(':');
     lcd.print(now.second(), DEC);
-
   }
 
+  // BME
   lcd.setCursor(0,1);
   lcd.print(bme.temperature);
   lcd.setCursor(6,1);
@@ -144,6 +120,7 @@ void loop() {
   lcd.setCursor(7,3);
   lcd.print("KOhms");
 
+  // UV
   lcd.setCursor(9,0);
   lcd.print(sensorVoltage);
   lcd.setCursor(14,0);
